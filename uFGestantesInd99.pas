ffd202176d4144c4c7f761d514564f8c10b23927
Marcelo M Gonçalves <61238770+marcelomgoncalves@users.noreply.github.com>
2023-11-06 09:33:20 -0300

    Add files via upload

--- uFGestantesInd99.pas
+++ uFGestantesInd99.pas
@@ -1,3 +1,30 @@
+//---------------------------------------------------------------------------//
+// Projeto: integraEsus
+// Sistema de integrador com ESUS para gestão de dados do programa PrevineBrasil
+//
+// Direitos Autorais Reservados (c) 2023 Marcelo M. Gonçalves
+//
+// Você pode obter a última versão desse arquivo no GitHub
+// URL: https://github.com/marcelomgoncalves/integradorESUS
+//
+// Este sistema é software livre; você pode redistribuí-la e/ou modificá-la
+// sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela
+// Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério)
+// qualquer versão posterior.
+//
+// Este sistema é distribuída na expectativa de que seja útil, porém, SEM
+// NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU
+// ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral
+// Menor do GNU para mais detalhes.
+//
+// Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto
+// com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,
+// no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+// Você também pode obter uma copia da licença em:
+// http://www.opensource.org/licenses/lgpl-license.php
+//
+// Marcelo M. Gonçalves - marcelomgoncalves@gmail.com
+//---------------------------------------------------------------------------//
 unit uFGestantesInd99;
 
 interface
